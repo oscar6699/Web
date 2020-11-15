44f89e9aa8b20fcf722529875cd666c899d5ae23
oscar6699 <os.munozv@alumnos.duoc.cl>
2020-11-14 21:59:15 -0300

    CREO Q LO SOLUCION

--- blog/settings.py
+++ blog/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.paht.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
